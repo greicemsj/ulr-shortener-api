397d553507d74be66ff3f9c800b8e2630a04e3ba
Greice Mara <sgreicemaraj@gmail.com>
2021-05-02 16:48:12 -0300

    update

---
+++ src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	
+	MONGO_CONNECTION:
+	'mongodb+srv://dbgreice:Rug5BrbMwkakQT6F@new-url.lhwdp.mongodb.net/new-url?retryWrites=true&w=majority',
+}

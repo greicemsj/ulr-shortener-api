0e9eacc2e157658fe8aeb5e672c4188ff0f0aa84
Greice Mara <51206170+greicemsj@users.noreply.github.com>
2021-05-03 12:52:46 -0300

    orientations

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -2,5 +2,5 @@ export const config = {
 	API_URL: 'http://localhost:5000',
 	
 	MONGO_CONNECTION:
-	'mongodb+srv://dbgreice:Rug5BrbMwkakQT6F@new-url.lhwdp.mongodb.net/new-url?retryWrites=true&w=majority',
+	'mongodb+srv://<user>:<password>@new-url.lhwdp.mongodb.net/<cluster-name>?retryWrites=true&w=majority',
 }
